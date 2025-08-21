1a4753f57eae8dfe620fbb2be72c97736392ae7c
deMouraPaulo <moura.paulo@aluno.unb.br>
2025-08-21 11:32:16 -0300

    Add files via upload

--- Core/MultiplexGain.m
+++ Core/MultiplexGain.m
@@ -3,7 +3,7 @@
 %
 % DESCRIPTION:
 %   Calculates the multiplexing gain based on the exact expression
-%   expressed in eq. (21a) (paper opportunistic FAMA).
+%   expressed in Eq. (21a) [*].
 %
 % INPUT:
 %   num_pool - (int) Number of users in the pool (M)
@@ -12,6 +12,11 @@
 %
 % OUTPUT:
 %   mg - (double)  multiplexing gain
+%
+% [*] K. -K. Wong, K. -F. Tong, Y. Chen, Y. Zhang and C. -B. Chae, 
+% "Opportunistic Fluid Antenna Multiple Access", 
+% IEEE Trans. Wireless Commun., vol. 22, no. 11, pp. 7819-7833, 
+% Nov. 2023, doi: 10.1109/TWC.2023.3255940.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
