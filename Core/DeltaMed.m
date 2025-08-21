f04a21db148d582f3413014493b4aecf66659a81
deMouraPaulo <moura.paulo@aluno.unb.br>
2025-08-21 11:39:07 -0300

    Update DeltaMed.m

--- Core/DeltaMed.m
+++ Core/DeltaMed.m
@@ -5,7 +5,7 @@
 function [dm] = DeltaMed (n,w)
 dm=0; 
 for k = 1:(n-1)
-dm = dm + (n-k) * besselj( 0, ( (2*pi*k/(n-1)) * w ) );
+    dm = dm + (n-k) * besselj( 0, ( (2*pi*k/(n-1)) * w ) );
 end
 dm = abs( dm * (2/(n*(n-1)) ) );
 end
@@ -14,4 +14,5 @@ end
 % Reference
 % [Ref. 1] K. -K. Wong, K. F. Tong, Y. Chen, and Y. Zhang, "Closed-form
 % expressions for spatial correlation parameters for performance
+
 % analysis of fluid antenna systems,” Electron. Lett., vol. 58, no.11, pp. 454–457, May 2022.
